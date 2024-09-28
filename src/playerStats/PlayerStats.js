f9492e874b1bce000c59c3867f35bd90e286d582
Ace of Spades <132790436+Verenthos@users.noreply.github.com>
2024-09-28 03:44:20 -0300

    Creating Avatar and XP Bar
    Creating Avatar and XP Bar

---
+++ src/playerStats/PlayerStats.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../styles/playerStats.css";
+
+const PlayerStats = () => {
+    return (
+        <div id="playerStats">
+            <h1 id="playerName">HECTOR SEIBEL LEVEL 26</h1>
+            <div id="playerStats__lines">
+                <span id="playerStats__lines__thick"></span>
+                <span id="playerStats__lines__thin"></span>
+            </div>
+            <h2 id="playerTitle">Fullstack Developer</h2>
+
+        </div>
+    );
+}
+
+export default PlayerStats;
