be853a0db4b90079acf3029568c6803f59ab0d54
Ace of Spades <132790436+Verenthos@users.noreply.github.com>
2024-09-29 10:02:37 -0300

    added subheading css
    added subheading css

--- src/about/AboutSubheading.js
+++ src/about/AboutSubheading.js
@@ -1,15 +1,21 @@
-import React, { Component } from 'react'
-import classNames from "classnames"
-import "../styles/aboutMenu.css"
+import React from 'react';
+import classNames from 'classnames';
+import '../styles/aboutMenu.css';
 
-const AboutSubheading = ({title, content, active, onClick, menuItem}) => {
-const subContainerClass = `sub-container-${menuItem}`
+const AboutSubheading = ({ title, content, active, onClick, menuItem }) => {
+  const subContainerClass = `sub-container-${menuItem}`;
+
   return (
-    <div className={classNames(subContainerClass, {"active-subheading": active})} >
-      <h3 >{onClick}{title}</h3>
-      <div className="p-container">{content}</div>
+    <div 
+      className={classNames(subContainerClass, { 'active-subheading': active })} 
+      onClick={onClick}  /* Add onClick to the div */
+    >
+      <h3>{title}</h3>
+      <div className={classNames('p-container', { 'active': active })}>
+        {content}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default AboutSubheading;
