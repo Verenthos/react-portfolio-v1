097ae980f55c78c124d0776061f7f3bc8f973697
Ace of Spades <132790436+Verenthos@users.noreply.github.com>
2024-09-28 23:23:31 -0300

    Subheading code
    Coded in the AboutmenuItem and Subheading

---
+++ src/about/subheadingsData.js
@@ -0,0 +1,59 @@
+const subheadingsData = {
+    1: [
+        {
+            title: "About me",
+            content: (
+                <>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas arcu mauris, pharetra vel viverra ac, luctus vehicula nibh. Sed tristique a nibh sit amet cursus. In vel accumsan ante, ultrices blandit nunc. Cras vestibulum nunc et nisl dapibus convallis. Sed mauris sem, faucibus quis tempor eget, iaculis in dui.</p>
+                </>
+            )
+        }
+    ],
+    2: [
+        {
+            title: "2016 Masters Degree",
+            content: (
+                <>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas arcu mauris, pharetra vel viverra ac, luctus vehicula nibh. Sed tristique a nibh sit amet cursus. In vel accumsan ante, ultrices blandit nunc. Cras vestibulum nunc et nisl dapibus convallis. Sed mauris sem, faucibus quis tempor eget, iaculis in dui.
+                </p>
+                </>
+            )
+        },
+        {
+            title: "2014 Bachelors Degree",
+            content: (
+                <>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas arcu mauris, pharetra vel viverra ac, luctus vehicula nibh. Sed tristique a nibh sit amet cursus. In vel accumsan ante, ultrices blandit nunc. Cras vestibulum nunc et nisl dapibus convallis. Sed mauris sem, faucibus quis tempor eget, iaculis in dui.
+                </p>
+                </>
+            )
+        }
+    ],
+    3: [
+        {
+            title: "2022 Front End Developer",
+            content: (
+                <>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas arcu mauris, pharetra vel viverra ac, luctus vehicula nibh. Sed tristique a nibh sit amet cursus. In vel accumsan ante, ultrices blandit nunc. Cras vestibulum nunc et nisl dapibus convallis. Sed mauris sem, faucibus quis tempor eget, iaculis in dui.
+                </p>
+                </>
+            )
+        },
+        {
+            title: "2020 UX Designer",
+            content: (
+                <>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas arcu mauris, pharetra vel viverra ac, luctus vehicula nibh. Sed tristique a nibh sit amet cursus. In vel accumsan ante, ultrices blandit nunc. Cras vestibulum nunc et nisl dapibus convallis. Sed mauris sem, faucibus quis tempor eget, iaculis in dui.
+                </p>
+                </>
+            )
+        }
+    ]
+}
+
+
+export default subheadingsData;
