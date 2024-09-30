7ffe0cfdc028a1838cb79f7d8550c171821d07e4
Ace of Spades <132790436+Verenthos@users.noreply.github.com>
2024-09-30 06:02:17 -0300

    skillsmenu
    added skillsmenu (must fix mobile res)

---
+++ src/skills/skillsData.js
@@ -0,0 +1,68 @@
+const skills = {
+    1: [ 
+        {
+        title: "HTML",
+        level: 6,
+        },
+        {
+        title: "CSS",
+        level: 6,
+        },
+        {
+        title: "SASS",
+        level: 6,
+        },
+        {
+        title: "JavaScript",
+        level: 5,
+        },
+        {
+        title: "TypeScript",
+        level: 4,
+        },
+        {
+        title: "Tailwind CSS",
+        level: 1,
+        },
+        {
+        title: "React JS",
+        level: 3,
+        },
+        {
+        title: "Figma",
+        level: 5,
+        },
+    ],
+    2: [ 
+        {
+        title: "PostgreSQL",
+        level: 6,
+        },
+        {
+        title: "Node.js",
+        level: 6,
+        },
+        {
+        title: "MongoDB",
+        level: 6,
+        },
+        {
+        title: "Git",
+        level: 5,
+        },
+        {
+        title: "PHP",
+        level: 1,
+        },
+        {
+        title: "MySQL",
+        level: 3,
+        },
+        {
+        title: "Express.js",
+        level: 3,
+        },
+    ],
+};
+
+export default skills;
