df585a6f940baf3342a61ccdd5687072a9a3fee6
Ace of Spades <132790436+Verenthos@users.noreply.github.com>
2024-10-01 04:12:59 -0300

    projects page
    add projects page (about menu might be on wrong place though)

---
+++ src/projects/projectsData.js
@@ -0,0 +1,44 @@
+import projectOne from "../assets/project-1.png";
+import projectTwo from "../assets/project-1.png";
+import projectThree from "../assets/project-1.png";
+
+const projects = {
+    1: {
+        title: "Social Media Platform",
+        image: projectOne,
+        description: (
+            <>
+                <>Some description talking about what was used to develop this project.</>
+            </>
+        ),
+        github: "https://github.com",
+        demo: "https://netlify.com",
+        
+    },
+    2: {
+        title: "Weather App",
+        image: projectTwo,
+        description: (
+            <>
+                <>Some description talking about what was used to develop this project.</>
+            </>
+        ),
+        github: "https://github.com",
+        demo: "https://netlify.com",
+        
+    },
+    3: {
+        title: "Books App",
+        image: projectThree,
+        description: (
+            <>
+                <>Some description talking about what was used to develop this project.</>
+            </>
+        ),
+        github: "https://github.com",
+        demo: "https://netlify.com",
+        
+    }
+}
+
+export default projects;
